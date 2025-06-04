3df9d6635dc1d1b7e481c5e30eabe7d77eeca649
zenoncoode <noah.aguiar@icloud.com>
2025-06-04 11:53:27 -0300

    Link para home na logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 // src/components/Header.tsx
 import React from "react";
 import styles from "./Header.module.css"; // Importando o CSS
+import {Link} from "react-router-dom"; // Importando Link do react-router-dom
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Devs.util</h1>
+      <Link to="/"><h1 className={styles.title}>Devs.util</h1></Link>
     </header>
   );
 }
